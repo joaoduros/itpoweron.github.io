ad4520c5183a5d9a81ea6093b02139ef2bed1b56
joaoduros <112966651+joaoduros@users.noreply.github.com>
2023-04-06 09:59:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function aviso() {
+    alert("Para fazer um pedido suporte poderá fazer através do email suporte@itpoweron.pt ou pelo número de telefone 218 059 428.\nO nosso horário de suporte nos dias úteis (segunda a sexta-feira, exceto feriados) é das 9h00 às 13h00 e das 14h30 às 18h30.")
+}
+
